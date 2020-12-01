671579cd580a77585d81d3585bd311af2734577f
Karina <karyraquell@outlook.com>
2020-12-01 10:57:49 -0500

    created javascript file

---
+++ src/script.js
@@ -0,0 +1,100 @@
+let dateToday = document.querySelector("h2.date");
+let currentTime = document.querySelector("h3.time");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+let current = new Date();
+
+let hour = current.getHours();
+
+let minutes = current.getMinutes();
+
+let year = current.getFullYear();
+
+let date = current.getDate();
+
+let day = days[current.getDay()];
+
+let month = months[current.getMonth()];
+
+dateToday.innerHTML = `${day}, ${month} ${date}, ${year}`;
+currentTime.innerHTML = `${hour}:${minutes}`;
+
+function displayCity(city) {
+  let apiKey = "039935d9f096c3e0803fd56649519a0c";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  displayCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+function displayTemp(response) {
+  console.log(response);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#high").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#low").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+}
+
+function showPosition(position) {
+  let apiKey = "039935d9f096c3e0803fd56649519a0c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function getCurrentPosition(event) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentCity = document.querySelector("#search-current");
+
+currentCity.addEventListener("click", getCurrentPosition);
+
+displayCity("Parma");
